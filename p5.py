69ffe30728037a896e2d001eeb9cd9e914f3687b
Jordan Sommers <jtsommer@ucsc.edu>
2015-05-04 23:46:52 -0700

    Remove hashing, apparently whatever python is already doing is more efficient yet still functional. Fix some bugs so plutonium runs

--- p5.py
+++ p5.py
@@ -69,45 +69,61 @@ class State():
 	"""An inventory state for the craftly minecraft crafting planner"""
 	def __init__(self, inventory):
 		self.inventory = Counter(inventory)
-		self.update()
+		# self.update()
 		
-	def __hash__(self):
-		# return frozenset(self.inventory.items()).__hash__()
-		return self.hash
+	# def __hash__(self):
+	# 	# return frozenset(self.inventory.items()).__hash__()
+	# 	return self.hash
 
-	def __eq__(self, other):
-		return self.hash is other.hash
+	# def __eq__(self, other):
+	# 	return self.hash is other.hash
 
 	def __str__(self):
 		return self.inventory.__str__()
 
 	def has_items(self, items):
-		for item, amount in items.items():
+		for item in items:
+			amount = items[item]
+
+			# Special case for Requires since the dictionary is True/False for a single item needed
+			# An integer amount falls through both cases
 			if amount is True:
 				amount = 1
 			elif amount is False:
 				amount = 0
+
 			if self.inventory[item] < amount:
 				return False
 		return True
 
-	def update(self):
-		self.hash = tuple(self.inventory.get(name, 0) for i,name in enumerate(Crafting.Items())).__hash__()
+	# def update(self):
+	# 	self.hash = tuple(self.inventory.get(name, 0) for i,name in enumerate(Crafting.Items())).__hash__()
+
+	def to_tuple(self):
+		return tuple(self.inventory.get(name, 0) for i,name in enumerate(Crafting.Items()))
+
+	def get_important_item_count(self, goalInv):
+		total_items_remaining = 0
+		for item in goalInv:
+			total_items_remaining += max(goalInv[item] - self.inventory[item], 0)
+		return total_items_remaining
 
 	def next_state(self, consumes, produces):
 		next = State(self.inventory)
-		for name, amount in consumes.items():
+		for name in consumes:
+			amount = consumes[name]
 			next.inventory[name] -= amount
-		for name, amount in produces.items():
+		for name in produces:
+			amount = produces[name]
 			next.inventory[name] += amount
-		next.update()
+		# next.update()
 		return next
 
 
-def make_RIKLS_heuristic():
+def make_RIKLS_heuristic(goal):
 	# discourage states with more than the necessary amount of items in inventory
 
-	maximums = {}
+	maximums = defaultdict(lambda: 1)
 	consumes = defaultdict(lambda: 1)
 	produces = defaultdict(lambda: 0)
 
@@ -181,10 +197,11 @@ def make_goal_checker(goal):
 		return state.has_items(goal)
 	return is_goal
 
-init = {}
-fin = {'stone_pickaxe': 1}
+init = Crafting.Initial()
+fin = Crafting.Goal()
 
 start = make_initial_state(init)
+end = make_initial_state(fin)
 is_goal = make_goal_checker(fin)
 
 print astar.search(Crafting.Graph(), start, is_goal, 35, make_RIKLS_heuristic())
