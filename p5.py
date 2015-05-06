a770fad562cbd3cad81625d3a122fcd34e678991
Derek Donahue <propolandante@gmail.com>
2015-05-06 13:22:35 -0700

    IT'S GOLD, JERRY!

--- p5.py
+++ p5.py
@@ -51,6 +51,40 @@ tool_check = {}
 for item in tool_reqs:
 	tool_check[item] = create_tool_check(tool_reqs[item])
 
+# Return a function that checks whether a recipe can be used at a given state
+def make_checker(rule):
+	# This runs once per recipe
+	# Write a checker function that examines rule['Consumes'] and rule['Requires']
+	requirements = {}
+	consumes = {}
+	if rule.has_key('Requires'):
+		requirements = rule['Requires']
+	if rule.has_key('Consumes'):
+		consumes = rule['Consumes']
+	def check(state):
+		# This runs a lot
+		# Should return whether or not this recipe can be executed
+		return has_items(state, requirements) and has_items(state, consumes)
+
+	return check
+
+# Return a function that generates the next state for a given recipe
+def make_effector(rule):
+	# This runs once per recipe
+	# Write an effector function that examines rule['Consumes'] and rule['Produces']
+	consumes = {}
+	produces = {}
+	if rule.has_key('Produces'):
+		produces = rule['Produces']
+	if rule.has_key('Consumes'):
+		consumes = rule['Consumes']
+	def effect(state):
+		# This runs a lot
+		# Should return the next state
+		return next_state(state, produces, consumes)
+
+	return effect
+
 class Crafting:
 	instance = None
 	"""A crafting module that defines recipes and items"""
@@ -120,62 +154,27 @@ class Crafting:
 	def RequirementsForItem(cls, item):
 		instance = cls.GetInstance()
 
+def magic_box(items = {}, produces = {}, consumes = {}):
+	item_list = Crafting.Items()
+	state = []
+	for item in item_list:
+		amount = items.get(item, 0) - consumes.get(item, 0) + produces.get(item, 0)
+		state.append((item, amount))
+	return tuple(state)
 
+empty_state = magic_box()
 		
+def has_items(state, items):
+	# print "state: ", state
+	# print "item_state: ", item_state
+	item_state = magic_box(items)
+	for i in range(len(state)):
+		if state[i][1] < item_state[i][1]:
+			return False
+	return True
 
-class State():
-	"""An inventory state for the craftly minecraft crafting planner"""
-	def __init__(self, inventory):
-		self.inventory = Counter(inventory)
-		# self.update()
-		
-	# def __hash__(self):
-	# 	# return frozenset(self.inventory.items()).__hash__()
-	# 	return self.hash
-
-	# def __eq__(self, other):
-	# 	return self.hash is other.hash
-
-	def __str__(self):
-		return self.inventory.__str__()
-
-	def has_items(self, items):
-		for item in items:
-			amount = items[item]
-
-			# Special case for Requires since the dictionary is True/False for a single item needed
-			# An integer amount falls through both cases
-			if amount is True:
-				amount = 1
-			elif amount is False:
-				amount = 0
-
-			if self.inventory[item] < amount:
-				return False
-		return True
-
-	# def update(self):
-	# 	self.hash = tuple(self.inventory.get(name, 0) for i,name in enumerate(Crafting.Items())).__hash__()
-
-	def to_tuple(self):
-		return tuple(self.inventory.get(name, 0) for i,name in enumerate(Crafting.Items()))
-
-	def get_important_item_count(self, goalInv):
-		total_items_remaining = 0
-		for item in goalInv:
-			total_items_remaining += max(goalInv[item] - self.inventory[item], 0)
-		return total_items_remaining
-
-	def next_state(self, consumes, produces):
-		next = State(self.inventory)
-		for name in consumes:
-			amount = consumes[name]
-			next.inventory[name] -= amount
-		for name in produces:
-			amount = produces[name]
-			next.inventory[name] += amount
-		# next.update()
-		return next
+def next_state(state, produces, consumes):
+	return magic_box(dict(state), produces, consumes)
 
 
 def make_RIKLS_heuristic(goal):
@@ -199,7 +198,8 @@ def make_RIKLS_heuristic(goal):
 		c = consumes[item]
 		# print item, p, c
 		# If current inventory is one less than required consumption, we would have to produce another batch
-		maximums[item] = p + max(c, goal.inventory[item]) - 1
+		# maximums[item] = p + max(c, goal.inventory[item]) - 1
+		maximums[item] = p + c - 1
 
 	print maximums
 
@@ -207,70 +207,38 @@ def make_RIKLS_heuristic(goal):
 	def RIKLS_heuristic(state):
 		tool_counter = 0
 		ingredient_counter = 0
-		for item in state.inventory:
-			amount = state.inventory[item]
+		for item, amount in state:
 			if amount > maximums[item]:
 				return float("inf")
 
 		# Doesn't help enough yet
-		for item in goal.inventory:
-			tool_counter += tool_check.get(item, lambda:0)(state)
-			goal_ingredients = Crafting.GetInstance().product_ingredients[item]
-			ingredient_counter += state.get_important_item_count(goal_ingredients)
+		# for item in goal.inventory:
+		# 	tool_counter += tool_check.get(item, lambda:0)(state)
+		# 	goal_ingredients = Crafting.GetInstance().product_ingredients[item]
+		# 	ingredient_counter += state.get_important_item_count(goal_ingredients)
 		
-		if tool_counter > 0:
-			return 10*tool_counter
-		elif not is_goal(state):
-			return ingredient_counter * 2
-		else:
-			return 0
+		# if tool_counter > 0:
+		# 	return 10*tool_counter
+		# elif not is_goal(state):
+		# 	return ingredient_counter * 2
+		# else:
+		# 	return 0
+		return 0
 
 	return RIKLS_heuristic
 
 def make_goal_heuristic(goal):
 	pass
 
-# Return a function that checks whether a recipe can be used at a given state
-def make_checker(rule):
-	# This runs once per recipe
-	# Write a checker function that examines rule['Consumes'] and rule['Requires']
-	requirements = {}
-	consumes = {}
-	if rule.has_key('Requires'):
-		requirements = rule['Requires']
-	if rule.has_key('Consumes'):
-		consumes = rule['Consumes']
-	def check(state):
-		# This runs a lot
-		# Should return whether or not this recipe can be executed
-		return state.has_items(requirements) and state.has_items(consumes)
 
-	return check
-
-# Return a function that generates the next state for a given recipe
-def make_effector(rule):
-	# This runs once per recipe
-	# Write an effector function that examines rule['Consumes'] and rule['Produces']
-	consumes = {}
-	produces = {}
-	if rule.has_key('Produces'):
-		produces = rule['Produces']
-	if rule.has_key('Consumes'):
-		consumes = rule['Consumes']
-	def effect(state):
-		# This runs a lot
-		# Should return the next state
-		return state.next_state(consumes, produces)
-
-	return effect
 
 def make_initial_state(inventory):
 	# Do something to make a state
-	return State(inventory)
+	return magic_box(inventory)
 
 def make_goal_checker(goal):
 	def is_goal(state):
-		return state.has_items(goal)
+		return has_items(state, goal)
 	return is_goal
 
 init = Crafting.Initial()
